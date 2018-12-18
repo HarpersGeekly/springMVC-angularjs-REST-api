8844881810cfd6d2105aeb31f786b91a134e5508
harpersgeekly <harperryanc@gmail.com>
2018-12-18 14:58:47 -0600

    Annotation @Repository

--- src/main/java/com/codstrainingapp/trainingapp/repositories/UsersRepository.java
+++ src/main/java/com/codstrainingapp/trainingapp/repositories/UsersRepository.java
@@ -6,7 +6,7 @@ import org.springframework.stereotype.Repository;
 
 import java.util.List;
 
-//@Repository
+@Repository
 public interface UsersRepository extends CrudRepository<User, Long> {
 
     User findByUsername(String username);
