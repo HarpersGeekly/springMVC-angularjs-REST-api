4877cd99e3e18a741c2eee17ffa88bf441b89aef
harpersgeekly <harperryanc@gmail.com>
2018-12-24 12:18:21 -0600

    adding crud methods into repo to be used in Service

--- src/main/java/com/codstrainingapp/trainingapp/repositories/UsersRepository.java
+++ src/main/java/com/codstrainingapp/trainingapp/repositories/UsersRepository.java
@@ -1,15 +1,23 @@
 package com.codstrainingapp.trainingapp.repositories;
 
 import com.codstrainingapp.trainingapp.models.User;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Repository;
 
 import java.util.List;
 
-@Repository
-public interface UsersRepository extends CrudRepository<User, Long> {
+public interface UsersRepository {
 
     User findByUsername(String username);
     User findByEmail(String email);
     List<User> findAll();
+    User findOne(long id);
+    void save(User user);
+    void update(User user);
+    void delete(User user);
+
+//    @Query(nativeQuery = true,
+//            value = "SELECT * FROM users WHERE id LIKE=?1")
+//    User findById(Long id);
 }
