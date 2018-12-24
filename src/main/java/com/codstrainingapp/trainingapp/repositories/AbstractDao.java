c3fe8e775cde2a970f6e545b0fe915bc1b99f8ae
harpersgeekly <harperryanc@gmail.com>
2018-12-24 12:18:44 -0600

    Generic class is the base class for all DAO implementation classes. It provides the wrapper methods for common hibernate operations

---
+++ src/main/java/com/codstrainingapp/trainingapp/repositories/AbstractDao.java
@@ -0,0 +1,49 @@
+package com.codstrainingapp.trainingapp.repositories;
+
+import org.hibernate.Criteria;
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+
+import java.io.Serializable;
+import java.lang.reflect.ParameterizedType;
+
+//http://websystique.com/springmvc/spring-4-mvc-and-hibernate4-integration-example-using-annotations/
+//This Generic class is the base class for all DAO implementation classes. It provides the wrapper methods for common hibernate operations
+public abstract class AbstractDao<PK extends Serializable, T> {
+
+    private final Class<T> persistentClass;
+
+    @SuppressWarnings("unchecked")
+    public AbstractDao(){
+        this.persistentClass =(Class<T>) ((ParameterizedType) this.getClass().getGenericSuperclass()).getActualTypeArguments()[1];
+    }
+
+    @Autowired
+    private SessionFactory sessionFactory;
+
+    protected Session getSession(){
+        return sessionFactory.getCurrentSession();
+    }
+
+    @SuppressWarnings("unchecked")
+    public T getByKey(PK key) {
+        return (T) getSession().get(persistentClass, key);
+    }
+
+    public void persist(T entity) {
+        getSession().persist(entity);
+    }
+
+    public void update(T entity) {
+        getSession().saveOrUpdate(entity);
+    }
+
+    public void delete(T entity) {
+        getSession().delete(entity);
+    }
+
+    protected Criteria createEntityCriteria(){
+        return getSession().createCriteria(persistentClass);
+    }
+}
