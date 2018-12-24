f5d3ad133855db6d777529d0aafac379582c575d
harpersgeekly <harperryanc@gmail.com>
2018-12-24 12:17:48 -0600

    created a UserService that autowires my UserRepository and implements the methods therein

---
+++ src/main/java/com/codstrainingapp/trainingapp/services/UserService.java
@@ -0,0 +1,55 @@
+package com.codstrainingapp.trainingapp.services;
+
+import com.codstrainingapp.trainingapp.models.User;
+import com.codstrainingapp.trainingapp.repositories.UsersRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.List;
+
+@Service
+@Transactional
+public class UserService {
+
+    @Autowired
+    private UsersRepository usersDao;
+
+    public UserService(UsersRepository usersDao) {
+        this.usersDao = usersDao;
+    }
+
+    public List<User> findAll() {
+        return usersDao.findAll();
+    }
+
+    public User findOne(long id) {
+        return usersDao.findOne(id);
+    }
+
+//    public User findOne(long id) {
+//        return (User) usersDao.findById(id);
+//    }
+
+    public User findByUsername(String username) {
+        return usersDao.findByUsername(username);
+    }
+
+    public User findByEmail(String email) {
+        return usersDao.findByEmail(email);
+    }
+
+    public void save(User user) {
+        usersDao.save(user);
+    }
+
+    public void update(User user) {
+        usersDao.update(user);
+    }
+
+    public void delete(User user) {
+        usersDao.delete(user);
+    }
+
+
+}
