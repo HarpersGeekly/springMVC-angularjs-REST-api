49c42b7710a5831927a11e8fd313908909f74d6f
harpersgeekly <harperryanc@gmail.com>
2018-12-28 11:58:19 -0600

    User has a JsonManagedReference annotation over posts which solves the Infinite Recursion issue

--- src/main/java/com/codstrainingapp/trainingapp/models/User.java
+++ src/main/java/com/codstrainingapp/trainingapp/models/User.java
@@ -1,22 +1,24 @@
 package com.codstrainingapp.trainingapp.models;
 
-import com.fasterxml.jackson.annotation.JsonIgnore;
+import com.fasterxml.jackson.annotation.*;
 import org.hibernate.validator.constraints.Email;
 import org.hibernate.validator.constraints.Length;
 import org.hibernate.validator.constraints.NotBlank;
-
 import javax.persistence.*;
+import javax.validation.constraints.Pattern;
 import java.time.LocalDateTime;
+import java.util.List;
 
-@Entity
-@Table(name="users")
+//JPA specification defines an object-relational mapping between tables in a relational database and a set of Java classes.
+@Entity(name="users") //is a POJO with mapping information. It's now a jpa entity object. Attributes then get automatically mapped to database columns with the same name
 public class User {
 
-    @Id
+    @Id // identifier, maps the primary key column
     @GeneratedValue(strategy = GenerationType.IDENTITY) // auto-increment
     private Long id;
 
     @Column(nullable = false, unique = true)
+    @Pattern(regexp = "(?=^.{3,20}$)^[a-zA-Z][a-zA-Z0-9 ]*[._-]?[a-zA-Z0-9 ]+$", message = "Username must be alphanumeric only.")
     @NotBlank(message="Please enter a username.")
     @Length(min = 2, max = 20, message="Your username must be between 2-20 characters.")
     private String username;
@@ -26,7 +28,6 @@ public class User {
     @Email(message = "That email is not a valid email address.")
     private String email;
 
-    @Column(nullable = true)
     @Length(max = 1500, message="Your bio must be less than 1500 characters.")
     private String bio;
 
@@ -41,25 +42,27 @@ public class User {
 
     public User(){}
 
-    public User(long id, String username, String email, String bio, String password, LocalDateTime date) {
+    public User(long id, String username, String email, String bio, String password, LocalDateTime date, List<Post> posts) {
         this.id = id;
         this.username = username;
         this.email = email;
         this.bio = bio;
         this.password = password;
         this.date = date;
+        this.posts = posts;
     }
 
-    public User(String username, String email, String password) {
+    public User(String username, String email, String bio) {
         this.username = username;
         this.email = email;
-        this.password = null;
+        this.bio = bio;
     }
 
 //    ============================= relationships ==========================
 
-//    @OneToMany
-//    public List<Post> posts;
+    @OneToMany(mappedBy = "user", cascade = CascadeType.ALL, fetch = FetchType.EAGER) //TODO double check why not to use EAGER vs LAZY
+    @JsonManagedReference // solves Infinite Recursion issue
+    private List<Post> posts;
 
 //    ============================ getters and setters =====================
 
@@ -110,5 +113,13 @@ public class User {
 
     public void setBio(String bio) {
         this.bio = bio;
+    }
+
+    public List<Post> getPosts() {
+        return posts;
+    }
+
+    public void setPosts(List<Post> posts) {
+        this.posts = posts;
     }
 }
