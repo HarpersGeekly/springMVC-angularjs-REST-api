80c938886cf248874e5f0ad5976dc7d2a07b5bb1
harpersgeekly <harperryanc@gmail.com>
2019-01-08 14:52:48 -0600

    Hibernate.initialize() for posts not working.

--- src/main/java/com/codstrainingapp/trainingapp/repositories/UsersRepositoryImpl.java
+++ src/main/java/com/codstrainingapp/trainingapp/repositories/UsersRepositoryImpl.java
@@ -2,7 +2,9 @@ package com.codstrainingapp.trainingapp.repositories;
 
 import com.codstrainingapp.trainingapp.models.User;
 import org.hibernate.Criteria;
+import org.hibernate.Hibernate;
 import org.hibernate.criterion.Restrictions;
+import org.hibernate.query.Query;
 import org.springframework.stereotype.Repository;
 import java.util.List;
 
@@ -12,7 +14,11 @@ public class UsersRepositoryImpl extends AbstractDao<Long, User> implements User
     @SuppressWarnings("unchecked")
     public List<User> findAll() {
         Criteria criteria = createEntityCriteria();
-        return (List<User>) criteria.list();
+        List<User> users = (List<User>) criteria.list();
+        for(User u : users) {
+            Hibernate.initialize(u.getPosts());
+        }
+        return users;
     }
 
     public User findByUsername(String username) {
@@ -28,11 +34,20 @@ public class UsersRepositoryImpl extends AbstractDao<Long, User> implements User
     }
 
     public User findOne(long id) {
-        return getByKey(id);
+        User user = getByKey(id);
+        Hibernate.initialize(user.getPosts());
+        Hibernate.initialize(user.getPostVotes());
+        return user;
     }
 
     public void save(User user) {
         persist(user);
+    }
+
+
+    public int totalPostVotes(Long id) {
+        Query query = createCustomQuery("FROM PostVote pv LEFT JOIN Post p ON p.id = pv.post_id inner JOIN User u ON u.id = p.user_id WHERE u.id LIKE ?1");
+        return query.getFetchSize();
     }
 
 }
