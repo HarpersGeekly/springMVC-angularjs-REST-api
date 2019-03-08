ee23ece4a331f0df4309ec7f673c2fe91f26facc
harpersgeekly <harperryanc@gmail.com>
2019-03-08 11:48:04 -0600

    Comment had a DTO counterpart

---
+++ src/main/java/com/codstrainingapp/trainingapp/models/CommentDTO.java
@@ -0,0 +1,54 @@
+package com.codstrainingapp.trainingapp.models;
+
+import java.time.LocalDateTime;
+
+public class CommentDTO {
+
+    private Long id;
+    private String body;
+    private LocalDateTime date;
+    private Post post;
+    private User user;
+
+    public CommentDTO(){}
+
+    public User getUser() {
+        return user;
+    }
+
+    public void setUser(User user) {
+        this.user = user;
+    }
+
+    public Post getPost() {
+        return post;
+    }
+
+    public void setPost(Post post) {
+        this.post = post;
+    }
+
+    public LocalDateTime getDate() {
+        return date;
+    }
+
+    public void setDate(LocalDateTime date) {
+        this.date = date;
+    }
+
+    public String getBody() {
+        return body;
+    }
+
+    public void setBody(String body) {
+        this.body = body;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+}
