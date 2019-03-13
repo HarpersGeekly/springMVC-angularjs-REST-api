5a487421f64112de3f59df46be3f0d4141745258
harpersgeekly <harperryanc@gmail.com>
2019-03-13 17:07:19 -0500

    Made a CommentRestController that handles routes to get comments by Post or User, and Save Comments

---
+++ src/main/java/com/codstrainingapp/trainingapp/controllers/CommentRestController.java
@@ -0,0 +1,53 @@
+package com.codstrainingapp.trainingapp.controllers;
+
+import com.codstrainingapp.trainingapp.models.CommentDTO;
+import com.codstrainingapp.trainingapp.services.CommentService;
+import com.codstrainingapp.trainingapp.utils.ResourceNotFoundException;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.time.LocalDateTime;
+import java.util.List;
+
+@RestController
+@RequestMapping("/api/comment")
+@CrossOrigin
+public class CommentRestController {
+
+    private CommentService commentSvc;
+
+    @Autowired
+    public CommentRestController(CommentService commentSvc) {
+        this.commentSvc = commentSvc;
+    }
+
+    @GetMapping("/commentsByPost/{postId}")
+    public List<CommentDTO> getCommentsByPost(@PathVariable Long postId) {
+        try {
+            commentSvc.findAllByPostId(postId);
+        } catch (Exception e) {
+            throw new ResourceNotFoundException();
+        }
+        return commentSvc.findAllByPostId(postId);
+    }
+
+    @GetMapping("/commentsByUser/{userId}")
+    public List<CommentDTO> getCommentsByUser(@PathVariable Long userId) {
+        try {
+            commentSvc.findAllByUserId(userId);
+        } catch (Exception e) {
+            throw new ResourceNotFoundException();
+        }
+        return commentSvc.findAllByUserId(userId);
+    }
+
+    @PostMapping("/saveComment")
+    @ResponseStatus(HttpStatus.CREATED)
+    public CommentDTO saveComment(@RequestBody CommentDTO comment) {
+        System.out.println("comment coming over " + comment);
+        comment.setDate(LocalDateTime.now());
+        return commentSvc.saveComment(comment);
+    }
+}
